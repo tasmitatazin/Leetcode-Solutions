16addb1ee8c01ce13a7c15856d4113ab0c56c5aa
Tasmita Tazin <tasmitatazin1@gmail.com>
2024-10-11 19:49:55 -0400

    Add files via upload

---
+++ Merge Sorted Array.cpp
@@ -0,0 +1,80 @@
+class Solution {
+public:
+
+    void merge(vector<int>& arr, int left, 
+                     int mid, int right)
+{
+    int n1 = mid - left + 1;
+    int n2 = right - mid;
+
+    // Create temp vectors
+    vector<int> L(n1), R(n2);
+
+    // Copy data to temp vectors L[] and R[]
+    for (int i = 0; i < n1; i++)
+        L[i] = arr[left + i];
+    for (int j = 0; j < n2; j++)
+        R[j] = arr[mid + 1 + j];
+
+    int i = 0, j = 0;
+    int k = left;
+
+    // Merge the temp vectors back 
+    // into arr[left..right]
+    while (i < n1 && j < n2) {
+        if (L[i] <= R[j]) {
+            arr[k] = L[i];
+            i++;
+        }
+        else {
+            arr[k] = R[j];
+            j++;
+        }
+        k++;
+    }
+
+    // Copy the remaining elements of L[], 
+    // if there are any
+    while (i < n1) {
+        arr[k] = L[i];
+        i++;
+        k++;
+    }
+
+    // Copy the remaining elements of R[], 
+    // if there are any
+    while (j < n2) {
+        arr[k] = R[j];
+        j++;
+        k++;
+    }
+}
+
+void mergeSort(vector<int>& arr, int left, int right)
+{
+    if (left >= right)
+        return;
+
+    int mid = left + (right - left) / 2;
+    mergeSort(arr, left, mid);
+    mergeSort(arr, mid + 1, right);
+    merge(arr, left, mid, right);
+}
+
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        //the two arrays to be sorted are nums 1 and nums 2
+        int p=0;
+        
+        for (int i=m;i<m+n;i++)
+        {
+            nums1[i]=nums2[p];
+            p++;
+        }
+
+        int z = nums1.size();
+
+        mergeSort(nums1, 0, z - 1);
+
+
+    }
+};
